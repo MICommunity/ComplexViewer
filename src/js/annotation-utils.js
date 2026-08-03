28b85a8be3d4a5f9852c77430fb8350922ce29b9
CLPF <carlastembio@gmail.com>
2026-08-03 10:57:33 +0100

    Use async functions instead of counter

--- src/js/annotation-utils.js
+++ src/js/annotation-utils.js
@@ -2,81 +2,81 @@ import * as d3 from "d3";
 import {Annotation} from "./viz/interactor/annotation";
 import {SequenceDatum} from "./viz/sequence-datum";
 
+// General and dynamic function to resolve UniProt IDs if they are not standard format
+async function resolveUniProtId(rawId) {
+    const uniprotAccRegex = new RegExp("^[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}$", "i");
+    if (uniprotAccRegex.test(rawId)) return rawId;
+
+    const searchUrl = `https://rest.uniprot.org/uniprotkb/search?query=gene:${rawId}&size=1`;
+    try {
+        const response = await d3.json(searchUrl);
+        const bestMatch = response?.results?.[0];
+        if (bestMatch && bestMatch.primaryAccession) {
+            return bestMatch.primaryAccession;
+        }
+    } catch (err) {
+        console.warn(`Could not resolve ID for gene ${rawId}:`, err);
+    }
+
+    return rawId;
+}
 
 //todo - cache annotations in memory
-export function fetchAnnotations(/*App*/ app, callback) {
+export async function fetchAnnotations(/*App*/ app, callback) {
     // we only show annotations on proteins
     const proteins = Array.from(app.participants.values()).filter(function (value) {
         return value.type === "protein";
     });
 
-    let protsAnnotated = 0;
-    const molCount = proteins.length;
+    const uniprotAccRegex = new RegExp("[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}![-]", "i");
+    const promises = [];
 
+    // Process all proteins concurrently using parallel promise handling
     for (let prot of proteins) {
-        protsAnnotated++;
-        const uniprotAccRegex = new RegExp("[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}![-]", "i");
-        const match = uniprotAccRegex.exec(prot.json.identifier.id);
-        if (match && match[0] === prot.json.identifier.id.trim()) {
-            getSuperFamFeatures(prot, () => {
-                if (protsAnnotated === molCount) callback();
-            });
-            getUniProtFeatures(prot, () => {
-                if (protsAnnotated === molCount) callback();
-            });
-            getDisProtFeatures(prot, () => {
-                if (protsAnnotated === molCount) callback();
-            });
-        }
+        const rawId = prot.json?.identifier?.id?.trim();
+        if (!rawId) continue;
+
+        promises.push(
+            resolveUniProtId(rawId).then(mappedId => {
+                const match = uniprotAccRegex.exec(mappedId);
+                if (match && match[0] === mappedId) {
+                    return Promise.all([
+                        getSuperFamFeatures(prot, mappedId),
+                        getUniProtFeatures(prot, mappedId),
+                        getDisProtFeatures(prot, mappedId),
+                        getAlphaFoldFeatures(prot, mappedId)
+                    ]);
+                }
+            })
+        );
     }
+
+    Promise.allSettled(promises).then(() => {
+        if (callback) callback();
+    });
 }
 
-function getUniProtFeatures(prot, callback) {
-    const url = `https://www.ebi.ac.uk/proteins/api/proteins/${prot.json.identifier.id.trim()}`;
-    d3.json(url).then(json => {
+function getUniProtFeatures(prot, id) {
+    const url = `https://www.ebi.ac.uk/proteins/api/proteins/${id}`;
+    return d3.json(url).then(json => {
         let annotations = prot.annotationSets.get("UniprotKB");
         if (typeof annotations === "undefined") {
             annotations = [];
             prot.annotationSets.set("UniprotKB", annotations);
         }
-        if (json) {
+        if (json && json.features) {
             for (let feature of json.features.filter(ft => ft.type === "DOMAIN")) {
                 const anno = new Annotation(feature.description, new SequenceDatum(null, `${feature.begin}-${feature.end}`));
                 annotations.push(anno);
             }
         }
-        callback();
     });
 }
 
-//const DISPROT_TYPE_TO_NAME = new Map([
-//    ['D', 'Disorder'],
-//    ['T', 'Transition'],
-//    ['F', 'Function']
-//]);
+async function getDisProtFeatures(prot, id) {
+    const url = `https://disprot.org/api/search?page_size=1&page=0&release=current&show_ambiguous=false&show_obsolete=false&acc=${id}`;
 
-//function getDisProtFeatures(prot, callback) {
-//    const url = `https://disprot.org/api/search?page_size=1&page=0&release=current&show_ambiguous=false&show_obsolete=false&acc=${prot.json.identifier.id.trim()}`;
-//    d3.json(url).then(json => {
-//        let annotations = prot.annotationSets.get("DisProt");
-//        if (typeof annotations === "undefined") {
-//            annotations = [];
-//            prot.annotationSets.set("DisProt", annotations);
-//        }
-//        if (json) {
-//            for (let feature of (json.data[0]?.['disprot_consensus']?.['Biological process'] || [])) {
-//                const anno = new Annotation(DISPROT_TYPE_TO_NAME.get(feature.type), new SequenceDatum(null, `${feature.start}-${feature.end}`));
-//                annotations.push(anno);
-//            }
-//        }
-//        callback();
-//    });
-//}
-
-function getDisProtFeatures(prot, callback) {
-    const url = `https://disprot.org/api/search?page_size=1&page=0&release=current&show_ambiguous=false&show_obsolete=false&acc=${prot.json.identifier.id.trim()}`;
-
-    d3.json(url).then(json => {
+    return d3.json(url).then(json => {
         let annotations = prot.annotationSets.get("DisProt");
         if (typeof annotations === "undefined") {
             annotations = [];
@@ -84,60 +84,53 @@ function getDisProtFeatures(prot, callback) {
         }
 
         const data = json?.data?.[0];
+        // Defensive validation (Null Safety) to prevent errors if regions are missing
+        if (!data || !data.regions) return;
+
         const consensus = data?.['disprot_consensus'];
+        const regions = data['regions'] || [];
 
         const regionNameMap = new Map(
-            (data?.['regions'] || [])
-                .map(region => [`${region.term_namespace}:${region.start}-${region.end}`, region.term_name])
+            regions.map(region => [`${region.term_namespace}:${region.start}-${region.end}`, region.term_name])
         );
 
         const namespaceToRegions = {
-            // Branch 1: Structural state
             'Structural state': [],
-
-            // Branch 2: Structural transition
             'Structural transition': [],
-
-            // Branch 3: Disorder function (we group all functions here)
             'Disorder function': [],
             'Biological process': [],
             'Molecular function': [],
             'Cellular component': []
         };
-        data.regions.forEach(region => namespaceToRegions[region.term_namespace].push(region));
+        regions.forEach(region => {
+            if (namespaceToRegions[region.term_namespace]) {
+                namespaceToRegions[region.term_namespace].push(region);
+            }
+        });
 
         function getDescription(namespace, feature) {
-            return regionNameMap.get(`${namespace}:${feature.start}-${feature.end}`) // Direct region match to consensus
-                || namespaceToRegions[namespace].find(region => region.start >= feature.start && region.end <= feature.end).term_name // Find consensus name for region
+            return regionNameMap.get(`${namespace}:${feature.start}-${feature.end}`)
+                || namespaceToRegions[namespace].find(region => region.start >= feature.start && region.end <= feature.end)?.term_name
                 || namespace;
         }
 
-        // Dictionary to group the JSON keys into the 3 ontology branches
-
-        // const FUNCTION_NAMESPACES = new Set(['Disorder function', 'Biological process', 'Molecular function', 'Cellular component']);
-
         if (consensus) {
-
-
-            for (const [namespace, regions] of Object.entries(namespaceToRegions)) {
+            for (const [namespace] of Object.entries(namespaceToRegions)) {
                 const features = consensus[namespace] || [];
 
                 for (let feature of features) {
-                    // We use namespace so ComplexViewer assigns the same color to everything that is a function
                     const region = `${feature.start}-${feature.end}`;
-                    // const groupedNamespace = FUNCTION_NAMESPACES.has(namespace) ? 'Function' : namespace;
                     const anno = new Annotation(namespace, new SequenceDatum(null, region), getDescription(namespace, feature));
                     annotations.push(anno);
                 }
             }
         }
-        callback();
     });
 }
 
-function getSuperFamFeatures(prot, callback) {
-    const url = `https://supfam.org/SUPERFAMILY/cgi-bin/das/up/features?segment=${prot.json.identifier.id.trim()}`;
-    d3.xml(url).then(xml => {
+async function getSuperFamFeatures(prot, id) {
+    const url = `https://supfam.org/SUPERFAMILY/cgi-bin/das/up/features?segment=${id}`;
+    return d3.xml(url).then(xml => {
         let annotations = prot.annotationSets.get("Superfamily");
         if (typeof annotations === "undefined") {
             annotations = [];
@@ -146,7 +139,7 @@ function getSuperFamFeatures(prot, callback) {
         if (xml) {
             const xmlFeatures = xml.getElementsByTagName("FEATURE");
             for (let xmlFeature of xmlFeatures) {
-                const type = xmlFeature.getElementsByTagName("TYPE")[0]; //might need to watch for text nodes getting mixed in here
+                const type = xmlFeature.getElementsByTagName("TYPE")[0];
                 const category = type.getAttribute("category");
                 if (category === "miscellaneous") {
                     const name = decodeHTML(type.getAttribute("id"));
@@ -156,7 +149,6 @@ function getSuperFamFeatures(prot, callback) {
                 }
             }
         }
-        callback();
     });
 }
 
