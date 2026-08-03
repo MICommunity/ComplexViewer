c01433cc55d0601518877764403f8a03bffad9f8
CLPF <carlastembio@gmail.com>
2026-08-03 10:58:57 +0100

    refactor string representation of annotations

--- src/js/viz/interactor/annotation.js
+++ src/js/viz/interactor/annotation.js
@@ -1,11 +1,18 @@
 export class Annotation {
     constructor(category, seqDatum, description = null) {
-        this.category = category.trim();
+        this.category = category ? String(category).trim() : "";
         this.seqDatum = seqDatum;
         this.description = description;
+
+        this.certain = null; // SVG Path set by Polymer.updatePositionalFeatures
+        this.fuzzyStart = null; // SVG Path set by Polymer.updatePositionalFeatures
+        this.fuzzyEnd = null; // SVG Path set by Polymer.updatePositionalFeatures
     }
 
     toString() {
-        return `${this.category} ${this.description ? ": " + this.description : ""} [${this.seqDatum ? this.seqDatum.toString() : `${this.seqDatum.begin} - ${this.seqDatum.end}`}]`;
+        const seqStr = this.seqDatum ? this.seqDatum.toString() : "N/A";
+        const descStr = this.description ? `: ${this.description}` : "";
+
+        return `${this.category}${descStr} [${seqStr}]`;
     }
 }
